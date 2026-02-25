ac278956439c2b3f4b91026bcf126b494a124ef6
roney ruiz <roneyruizrojas@gmail.com>
2026-02-24 21:21:24 -0600

    feat: add ping endpoint

--- source/Entrypoint.ts
+++ source/Entrypoint.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia()
-  .get("/", () => "Hello Elysia")
+  .get("/", () => ({ ping: "pong" }))
   .listen(3000);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
