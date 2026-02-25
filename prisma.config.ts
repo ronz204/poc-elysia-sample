5ffc65ad3836aeb45ce2ccf29ed9bb3916f949c1
roney ruiz <roneyruizrojas@gmail.com>
2026-02-24 21:19:54 -0600

    feat: initialize prisma with sqlite

---
+++ prisma.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/",
+  migrations: {
+    path: "prisma/Migrations",
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
