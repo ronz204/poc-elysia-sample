ad65e858f17c5268b43c3b5fff2040be22bcbb53
roney ruiz <roneyruizrojas@gmail.com>
2026-02-25 21:23:09 -0600

    refac: move pagination to spec base

--- prisma/Specs/Specification.ts
+++ prisma/Specs/Specification.ts
@@ -1,3 +1,16 @@
 export abstract class Specification {
+  private DEFAULT_PAGE = 1;
+  private DEFAULT_LIMIT = 10;
+
   abstract toQuery(): any;
+
+  protected toPaginate(query: any) {
+    const page = query.page ?? this.DEFAULT_PAGE;
+    const limit = query.limit ?? this.DEFAULT_LIMIT;
+
+    const skip = (page - 1) * limit;
+    const take = limit;
+
+    return { skip, take };
+  };
 };
