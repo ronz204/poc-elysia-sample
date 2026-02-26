3145efd11a3b4d396fe6a3c7217f5c743edf08bd
roney ruiz <roneyruizrojas@gmail.com>
2026-02-25 21:24:50 -0600

    feat: add retrieve query to user schemas

--- source/UserSchema.ts
+++ source/UserSchema.ts
@@ -1,9 +1,22 @@
 import { t } from "elysia";
 
-export const ListUser200Response = t.Object({
+const RetrieveQuery = t.Object({
+  page: t.Optional(t.Number()),
+  limit: t.Optional(t.Number()),
+});
+
+const Status200 = t.Object({
   users: t.Array(t.Object({
     id: t.Number(),
     name: t.String(),
     email: t.String()
   }))
 });
+
+export const RetrieveUsersSchemas = {
+  query: RetrieveQuery,
+  responses: [Status200],
+};
+
+export type RetrieveUsers200 = typeof Status200.static;
+export type RetrieveUserQuery = typeof RetrieveQuery.static;
