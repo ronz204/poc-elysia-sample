180e97e8aebd5a350a86bce90eda744a5c991148
roney ruiz <roneyruizrojas@gmail.com>
2026-02-25 21:25:05 -0600

    feat: implement specs to user servie

--- source/UserService.ts
+++ source/UserService.ts
@@ -1,10 +1,11 @@
-import { PrismaClient } from "@Prisma/client";
 import type { User } from "@Prisma/client";
+import { PrismaClient } from "@Prisma/client";
+import { Specification } from "@Specs/Specification";
 
 export class UserService {
   constructor(private prisma: PrismaClient) {};
 
-  public async listUsers(): Promise<User[]> {
-    return this.prisma.user.findMany();
+  public async retrieve(spec: Specification): Promise<User[]> {
+    return this.prisma.user.findMany(spec.toQuery());
   };
 };
