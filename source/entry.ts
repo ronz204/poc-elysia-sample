c9152623f44e0e269503e49c6d1ca6ab5d00cbf2
roney ruiz <roneyruizrojas@gmail.com>
2026-03-04 21:27:15 -0600

    chore: suspend modules temporary

--- source/entry.ts
+++ source/entry.ts
@@ -1,10 +1,7 @@
 import { Elysia } from "elysia";
-import { ErrorsPlugin } from "@plugins/errors.plugin";
-import { UsersPlugin } from "@modules/users/users.plugin";
+
 
 const app = new Elysia()
-  .use(ErrorsPlugin)
-  .use(UsersPlugin)
   .listen(3000);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
