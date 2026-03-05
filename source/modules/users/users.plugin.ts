859d6e414fa3cb120ed5576b7991e9d685835fa3
roney ruiz <roneyruizrojas@gmail.com>
2026-03-04 21:45:54 -0600

    chore: purge all modules temporary

--- source/modules/users/users.plugin.ts
+++
@@ -1,7 +0,0 @@
-import { RetrievePlugin } from "./retrieve/retrieve.plugin";
-import { CreatePlugin } from "./create/create.plugin";
-import { Elysia } from "elysia";
-
-export const UsersPlugin = new Elysia({ prefix: "/users" })
-  .use(RetrievePlugin)
-  .use(CreatePlugin);
