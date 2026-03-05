9c2fec807925104bf9e2addf0c3be9a2f8d50d2c
roney ruiz <roneyruizrojas@gmail.com>
2026-03-04 22:32:23 -0600

    chore: move prisma and specify to database layer

---
+++ source/database/specify.base.ts
@@ -0,0 +1,21 @@
+export abstract class Specify {
+  private DEFAULT_PAGE = 1;
+  private DEFAULT_LIMIT = 10;
+
+  abstract toQuery(): any;
+
+  protected toPaginate(query: Paginated) {
+    const page = query.page ?? this.DEFAULT_PAGE;
+    const limit = query.limit ?? this.DEFAULT_LIMIT;
+
+    const skip = (page - 1) * limit;
+    const take = limit;
+
+    return { skip, take };
+  };
+};
+
+interface Paginated {
+  page?: number;
+  limit?: number;
+};
