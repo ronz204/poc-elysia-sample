93e37132e679a5cc25dc9ec1af96660bdf3b31e1
roney ruiz <roneyruizrojas@gmail.com>
2026-03-05 22:12:51 -0600

    feat: complete create new user handler and plugin

---
+++ source/features/users/handlers/create.handler.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import { ExistsSpecify } from "../prisma/exists.specify";
+import { CreateSpecify } from "../prisma/create.specify";
+
+import type { Handler } from "@contracts/handler.contract";
+import type { CreateCommand } from "../schemas/create.schema";
+import type { CreateResponse } from "../schemas/create.schema";
+
+export class CreateHandler implements Handler<CreateCommand, CreateResponse> {
+  constructor(private readonly prisma: PrismaClient) {};
+
+  public async handle(command: CreateCommand): Promise<CreateResponse> {
+    const existsQuery = new ExistsSpecify(command).toQuery();
+    const exists = await this.prisma.user.findFirst(existsQuery);
+
+    const createQuery = new CreateSpecify(command).toQuery();
+    return await this.prisma.user.create(createQuery);
+  };
+};
--- source/features/users/user.plugin.ts
+++ source/features/users/user.plugin.ts
@@ -1,5 +1,7 @@
 import { Elysia } from "elysia";
+import { CreatePlugin } from "./plugins/create.plugin";
 import { RetrievePlugin } from "./plugins/retrieve.plugin";
 
 export const UsersPlugin = new Elysia({ prefix: "/users" })
+  .use(CreatePlugin)
   .use(RetrievePlugin);
