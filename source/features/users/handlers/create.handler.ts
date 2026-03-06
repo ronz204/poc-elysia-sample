544585ea8f9f4a177160a6bb2b4675e5d438eb62
roney ruiz <roneyruizrojas@gmail.com>
2026-03-05 22:56:17 -0600

    feat: implemente user already exists error

--- source/features/users/handlers/create.handler.ts
+++ source/features/users/handlers/create.handler.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { UserAlreadyExistsError } from "../user.errors";
 import { ExistsSpecify } from "../prisma/exists.specify";
 import { CreateSpecify } from "../prisma/create.specify";
 
@@ -11,7 +12,9 @@ export class CreateHandler implements Handler<CreateCommand, CreateResponse> {
 
   public async handle(command: CreateCommand): Promise<CreateResponse> {
     const existsQuery = new ExistsSpecify(command).toQuery();
+    
     const exists = await this.prisma.user.findFirst(existsQuery);
+    if (exists) throw new UserAlreadyExistsError("User with these credentials already exists");
 
     const createQuery = new CreateSpecify(command).toQuery();
     return await this.prisma.user.create(createQuery);
