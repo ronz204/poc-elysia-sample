7c32f0b8e10da528f9d8c4f04cd733c714ed5a8a
roney ruiz <roneyruizrojas@gmail.com>
2026-03-06 21:18:03 -0600

    feat: implement logixlysia example

--- source/entry.ts
+++ source/entry.ts
@@ -1,11 +1,24 @@
 import { Elysia } from "elysia";
+import { logixlysia } from "logixlysia";
 import { ErrorsPlugin } from "@plugins/errors.plugin";
 import { UsersPlugin } from "./features/users/user.plugin";
 
 const app = new Elysia()
+  .use(logixlysia({
+    config: {
+      showStartupMessage: true,
+      startupMessageFormat: "banner",
+
+      pino: {
+        level: "info",
+        prettyPrint: {
+          colorize: true,
+          translateTime: 'HH:MM:ss Z',
+          ignore: 'pid,hostname'
+        },
+      },
+    },
+  }))
   .use(ErrorsPlugin)
   .use(UsersPlugin)
   .listen(3000);
-
-const url = `http://${app.server?.hostname}:${app.server?.port}`;
-console.log(`🦊 Elysia is running at ${url}`);
