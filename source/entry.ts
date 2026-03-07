997af760a38292648a8dcf9b9f19f24618c2fc64
roney ruiz <roneyruizrojas@gmail.com>
2026-03-06 21:26:06 -0600

    feat: extract pino to create global logger

--- source/entry.ts
+++ source/entry.ts
@@ -1,22 +1,27 @@
+import { pino } from  "pino";
 import { Elysia } from "elysia";
 import { logixlysia } from "logixlysia";
 import { ErrorsPlugin } from "@plugins/errors.plugin";
 import { UsersPlugin } from "./features/users/user.plugin";
 
+export const logger = pino({
+  level: "info",
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'HH:MM:ss Z',
+      ignore: 'pid,hostname',
+    },
+  },
+});
+
 export const app = new Elysia()
   .use(logixlysia({
     config: {
+      pino: logger,
       showStartupMessage: true,
       startupMessageFormat: "banner",
-
-      pino: {
-        level: "info",
-        prettyPrint: {
-          colorize: true,
-          translateTime: 'HH:MM:ss Z',
-          ignore: 'pid,hostname'
-        },
-      },
     },
   }))
   .use(ErrorsPlugin)
