d0e351065ffc4939377823e216858389303c18e2
roney ruiz <roneyruizrojas@gmail.com>
2026-03-08 20:54:55 -0600

    feat: complete remove user handler and plugin

---
+++ source/features/users/plugins/remove.plugin.ts
@@ -0,0 +1,19 @@
+import { Elysia } from "elysia";
+import { PrismaPlugin } from "@database/prisma.plugin";
+import { RemoveSchema } from "../schemas/remove.schema";
+import { RemoveHandler } from "../handlers/remove.handler";
+
+export const RemovePlugin = new Elysia()
+  .use(PrismaPlugin)
+
+  .decorate(({ prisma }) => ({
+    removeH: new RemoveHandler(prisma),
+  }))
+
+  .delete("/", async ({ body, status, removeH }) => {
+    const response = await removeH.handle(body);
+    return status(200, response);
+  }, {
+    body: RemoveSchema.body,
+    response: RemoveSchema.response,
+  });
--- source/features/users/user.plugin.ts
+++ source/features/users/user.plugin.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
 import { CreatePlugin } from "./plugins/create.plugin";
+import { RemovePlugin } from "./plugins/remove.plugin";
 import { RetrievePlugin } from "./plugins/retrieve.plugin";
 
 export const UsersPlugin = new Elysia({ prefix: "/users" })
   .use(CreatePlugin)
+  .use(RemovePlugin)
   .use(RetrievePlugin);
