68555ab5d9c467858b5da42cbda6c86d889bda05
roney ruiz <roneyruizrojas@gmail.com>
2026-03-08 20:53:01 -0600

    feat: create user not found concrete error

--- source/features/users/user.errors.ts
+++ source/features/users/user.errors.ts
@@ -3,4 +3,8 @@ export const UserErrors = {
     code: "USER_ALREADY_EXISTS",
     message: "User with these credentials already exists",
   },
+  NOT_FOUND: {
+    code: "USER_NOT_FOUND",
+    message: "User with these credentials not found",
+  },
 };
