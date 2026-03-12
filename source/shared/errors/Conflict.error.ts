9738729ed34c38aa6ca9368cbf6f74d09ddf6960
roney ruiz <roneyruizrojas@gmail.com>
2026-03-12 00:29:12 -0600

    chore: purge errors plugin and conflict error

--- source/shared/errors/Conflict.error.ts
+++
@@ -1,8 +0,0 @@
-import type { ErrorArgs } from "@contracts/Errors.contract";
-import { BaseError } from "@contracts/Errors.contract";
-
-export class ConflictError extends BaseError {
-  constructor(args: ErrorArgs) {
-    super(409, args);
-  };
-};
