374bd77d21001bc9df8d98035cefa5b63c823c61
roney ruiz <roneyruizrojas@gmail.com>
2026-03-12 00:44:50 -0600

    feat: implement plugins

--- source/boot.ts
+++ source/boot.ts
@@ -1,4 +1,10 @@
 import { Elysia } from "elysia";
+import { LogixPlugin } from "@plugins/logger.plugin";
+import { ErrorsPlugin } from "@plugins/errors.plugin";
+import { HealthPlugin } from "@plugins/health.plugin";
 
-export const app = new Elysia()
+export const app = new Elysia({ prefix: "/api" })
+  .use(ErrorsPlugin)
+  .use(LogixPlugin)
+  .use(HealthPlugin)
   .listen(3000);
