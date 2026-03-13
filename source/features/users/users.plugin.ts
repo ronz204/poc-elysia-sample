b81dc955ae631d7a0dcdeecef2653e7bbf1e9362
roney ruiz <roneyruizrojas@gmail.com>
2026-03-12 22:28:47 -0600

    feat: complete users plugin feature

--- source/features/users/users.plugin.ts
+++ source/features/users/users.plugin.ts
@@ -1,7 +1,9 @@
 import { Elysia, type ElysiaConfig } from "elysia";
+import { RegisterPlugin } from "./core/register/register.plugin";
 
 const config: ElysiaConfig<"/users"> = {
   prefix: "/users", name: "users.plugin",
 };
 
-export const UsersPlugin = new Elysia(config);
+export const UsersPlugin = new Elysia(config)
+  .use(RegisterPlugin);
