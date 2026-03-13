1249fe1592458534b731ad4b4f61a0510d8d645f
roney ruiz <roneyruizrojas@gmail.com>
2026-03-12 22:48:45 -0600

    chore: rename command by request in register user handler

--- source/features/users/core/register/register.handler.ts
+++ source/features/users/core/register/register.handler.ts
@@ -12,13 +12,13 @@ import type { RegisterResponse } from "./register.schema";
 export class RegisterHandler implements Handler<RegisterRequest, RegisterResponse> {
   constructor(private readonly prisma: PrismaClient) {};
 
-  public async handle(command: RegisterRequest): Promise<RegisterResponse> {
-    const existsQuery = new ExistsSpecify(command.body).toQuery();
+  public async handle(request: RegisterRequest): Promise<RegisterResponse> {
+    const existsQuery = new ExistsSpecify(request.body).toQuery();
 
     const exists = await this.prisma.user.findFirst(existsQuery);
     if (exists) throw new ConflictError(UserErrors.ALREADY_EXISTS);
 
-    const registerQuery = new RegisterSpecify(command).toQuery();
+    const registerQuery = new RegisterSpecify(request).toQuery();
     return await this.prisma.user.create(registerQuery);
   };
 };
