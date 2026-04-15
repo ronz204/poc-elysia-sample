13875450ea3d63344aa66ee089cceb482cb32ee5
roney ruiz <roneyruizrojas@gmail.com>
2026-04-14 21:52:43 -0600

    chore: adjust tspath resolution

--- source/boot.ts
+++ source/boot.ts
@@ -1,6 +1,8 @@
 import { Elysia } from "elysia";
+import { CorsPlugin } from "@plugins/cors.plugin";
 
 export const app = new Elysia({ prefix: "/api" })
+  .use(CorsPlugin)
   .listen(3000);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
