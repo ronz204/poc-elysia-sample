8178ff0d160b8410c8dd16f5dda5dd31c883b7f6
roney ruiz <roneyruizrojas@gmail.com>
2026-04-14 21:53:52 -0600

    feat: create healthcheck plugin

--- source/boot.ts
+++ source/boot.ts
@@ -1,8 +1,10 @@
 import { Elysia } from "elysia";
 import { CorsPlugin } from "@plugins/cors.plugin";
+import { HealthPlugin } from "@plugins/health.plugin";
 
 export const app = new Elysia({ prefix: "/api" })
   .use(CorsPlugin)
+  .use(HealthPlugin)
   .listen(3000);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
