7cff9f84062ebbefad0485e8f105462cd48dcb18
roney ruiz <roneyruizrojas@gmail.com>
2026-04-14 21:50:56 -0600

    deps: install and config elysia cors

---
+++ source/plugins/cors.plugin.ts
@@ -0,0 +1,7 @@
+import cors from "@elysiajs/cors";
+
+export const CorsPlugin = cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
