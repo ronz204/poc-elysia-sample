e5ce67213e8bd14bb285cb45cdafef473a82e07f
roney ruiz <roneyruizrojas@gmail.com>
2026-04-14 21:44:12 -0600

    deps: update prisma and libsql adaptar

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   schema: "prisma/",
   migrations: {
     path: "prisma/migrations",
-    seed: "bun prisma/seeds/seeder.ts",
   },
   datasource: {
     url: env("DATABASE_URL"),
