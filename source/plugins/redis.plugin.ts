44090c3427a384b9e2e99e01beaac81fd6447988
roney ruiz <roneyruizrojas@gmail.com>
2026-04-27 21:29:17 -0600

    chore: move prisma plugin and delete redis

--- source/plugins/redis.plugin.ts
+++
@@ -1,8 +0,0 @@
-import { Elysia } from "elysia";
-import { RedisClient } from "bun";
-
-const name: string = "redis.plugin";
-const url = process.env.REDIS_URL ?? "";
-
-export const RedisPlugin = new Elysia({ name })
-  .decorate("redis", new RedisClient(url));
