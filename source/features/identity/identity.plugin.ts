019ae54cf0f559250431ffea9aa4b43bc454b02e
roney ruiz <roneyruizrojas@gmail.com>
2026-04-27 21:35:26 -0600

    chore: rename identity plugin

--- source/features/identity/identity.plugin.ts
+++ source/features/identity/identity.plugin.ts
@@ -3,7 +3,7 @@ import { SignInUserPlugin } from "./signin-user/signin-user.plugin";
 import { SignUpUserPlugin } from "./signup-user/signup-user.plugin";
 
 const prefix: string = "/auth";
-const name: string = "identity.plugin";
+const name: string = "auth.plugin";
 
 export const IdentityPlugin = new Elysia({ name, prefix })
   .use(SignInUserPlugin)
