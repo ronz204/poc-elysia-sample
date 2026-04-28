3d7f92ce08e77ea1b50624d242dd830452aeb74a
roney ruiz <roneyruizrojas@gmail.com>
2026-04-27 21:30:12 -0600

    chore: improve user idao typing

--- source/core/dal/users/user.idao.ts
+++ source/core/dal/users/user.idao.ts
@@ -2,7 +2,9 @@ import type { User } from "@prisma/client";
 import { Create } from "./queries/create.query";
 import { Obtain } from "./queries/obtain.query";
 
+type UserNull = User | null;
+
 export interface IUserDao {
   create(args: Create.Args): Promise<User>;
-  obtain(args: Obtain.Args): Promise<User | null>;
+  obtain(args: Obtain.Args): Promise<UserNull>;
 };
