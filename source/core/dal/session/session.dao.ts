08409303b56df594831aa46de4acbd33b8da30db
roney ruiz <roneyruizrojas@gmail.com>
2026-04-30 12:15:22 -0600

    feat: write obtain unique session query

--- source/core/dal/session/session.dao.ts
+++ source/core/dal/session/session.dao.ts
@@ -1,16 +1,21 @@
 import type { ISessionDao } from "./session.idao";
 import { PrismaClient } from "@prisma/client";
+import { Obtain } from "./queries/obtain.query";
 import { Create } from "./queries/create.query";
 import { Revoke } from "./queries/revoke.query";
 
 export class SessionDao implements ISessionDao {
   constructor(private prisma: PrismaClient) {};
 
-  public async create(args: Create.Args) {
+  public async create(args: Create.Args): Promise<void> {
     await this.prisma.session.create(Create.query(args));
   };
 
-  public async revoke(args: Revoke.Args) {
+  public async revoke(args: Revoke.Args): Promise<void> {
     await this.prisma.session.update(Revoke.query(args));
+  };
+
+  public async obtain(args: Obtain.Args): Promise<Obtain.Result | null> {
+    return await this.prisma.session.findUnique(Obtain.query(args));
   };
 };
--- source/core/dal/session/session.idao.ts
+++ source/core/dal/session/session.idao.ts
@@ -1,7 +1,9 @@
+import { Obtain } from "./queries/obtain.query";
 import { Create } from "./queries/create.query";
 import { Revoke } from "./queries/revoke.query";
 
 export interface ISessionDao {
   create(args: Create.Args): Promise<void>;
   revoke(args: Revoke.Args): Promise<void>;
+  obtain(args: Obtain.Args): Promise<Obtain.Result | null>;
 };
