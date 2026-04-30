9a3fddeb70735fe3d56d14b18ce62eb69c47270c
roney ruiz <roneyruizrojas@gmail.com>
2026-04-29 22:10:25 -0600

    deps: install user agent parser

--- source/boot.ts
+++ source/boot.ts
@@ -1,14 +1,12 @@
 import { Elysia } from "elysia";
 import { AppConfig } from "@configs/app.config";
 import { CorsPlugin } from "@plugins/cors.plugin";
-import { ErrorPlugin } from "@plugins/errors.plugin";
 import { HealthPlugin } from "@plugins/health.plugin";
 import { ScalarPlugin } from "@plugins/scalar.plugin";
 
 import { IdentityPlugin } from "@features/identity/identity.plugin";
 
 export const app = new Elysia({ prefix: "/api" })
-  .use(ErrorPlugin)
   .use(CorsPlugin)
   .use(HealthPlugin)
   .use(ScalarPlugin)
