d39dc6e2df5962f9a8d6d010382990a7b2b9ffec
roney ruiz <roneyruizrojas@gmail.com>
2026-04-30 11:48:22 -0600

    feat: add refresh ttl variable to auth config

--- source/shared/configs/auth.config.ts
+++ source/shared/configs/auth.config.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
   JWT_SECRET: z.string().min(8),
+  REFRESH_TTL: z.number().default(7 * 24 * 60 * 60),
 });
 
 export const AuthConfig = envSchema.parse(process.env);
