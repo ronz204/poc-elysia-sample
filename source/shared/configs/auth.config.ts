4e82469cf36693c60343170ad08dfca20ca3a601
roney ruiz <roneyruizrojas@gmail.com>
2026-04-30 11:54:20 -0600

    feat: add access ttl for expiration

--- source/shared/configs/auth.config.ts
+++ source/shared/configs/auth.config.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  JWT_SECRET: z.string().min(8),
+  SECRET: z.string().min(8),
+  ACCESS_TTL: z.string().default("15m"),
   REFRESH_TTL: z.number().default(7 * 24 * 60 * 60),
 });
 
