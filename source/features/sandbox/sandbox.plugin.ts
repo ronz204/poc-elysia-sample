18841672f6dbeb085776659a81f51cca421911f4
roney ruiz <roneyruizrojas@gmail.com>
2026-05-03 15:55:24 -0600

    feat: create sample using ws and access verification

--- source/features/sandbox/sandbox.plugin.ts
+++ source/features/sandbox/sandbox.plugin.ts
@@ -5,6 +5,18 @@ import { AccessPlugin } from "@auth/access/access.plugin";
 export const SandboxPlugin = new Elysia({ name: "sandbox.plugin" })
   .use(AccessPlugin)
 
+  .ws("/socket", {
+    withWsAccess: true,
+
+    open: async (ws) => {
+      console.log(`WebSocket connection established: ${ws.id}`);
+    },
+
+    close: async (ws) => {
+      console.log(`WebSocket connection closed: ${ws.id}`);
+    },
+  })
+
   .get("/sandbox", async ({ status, userId }) => {
     console.log(`Sandbox accessed by user with ID: ${userId}`);
     return status(200, { message: "Sandbox accessed successfully!" });
